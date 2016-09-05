e5f8958e8ccd101f0400956f2e410bd7a2e7e854
Asad Shaheen Qazi <therogueartist@outlook.com>
2016-09-04 22:47:24 -0700

    Add files via upload

---
+++ 1000Hellos.java
@@ -0,0 +1,16 @@
+
+public class 1000Hellos {
+
+    public static void main(String[] args) {
+        
+        double H = 1000;
+        
+       for(int i=1; i<=H;i++){  
+      if (i==1 || i%10==1 && i!= 11) System.out.println(i + "st Hello.");
+       else if (i==2 || i%10==2 && i!= 12) System.out.println(i + "nd Hello.");
+      else if (i==3 || i%10==3 && i != 13) System.out.println(i + "rd Hello");
+       else System.out.println(i + "th Hello.");
+                                      
+    }
+}
+}
